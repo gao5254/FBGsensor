8ceb1915127587e85d66dcb1207503f023147d30
gao5254 <gao5254@qq.com>
2018-09-26 22:40:58 +0800

    界面控件程序实现1

--- FBGsensor/fbgsensor.cpp
+++ FBGsensor/fbgsensor.cpp
@@ -4,4 +4,28 @@ FBGsensor::FBGsensor(QWidget *parent)
 	: QMainWindow(parent)
 {
 	ui.setupUi(this);
+
+	//初始状态下某些输入框不可用
+	ui.setParaBtn->setEnabled(false);
+	ui.wavEndEdit->setEnabled(false);
+	ui.wavStartEdit->setEnabled(false);
+	ui.wavStepEdit->setEnabled(false);
+}
+
+FBGsensor::~FBGsensor()
+{
+//TODO:
+// 如果串口未关闭，关闭串口
+
+}
+
+void FBGsensor::on_openDeviceBtn_clicked()
+{
+//TODO:
+// 按钮和工具栏动作均调用此槽函数
+// 1.获取端口号信息
+// 2.打开端口
+// 3.获取设备信息
+// 4.成功后解锁其他输入框及按钮
+
 }
--- FBGsensor/fbgsensor.h
+++ FBGsensor/fbgsensor.h
@@ -9,7 +9,15 @@ class FBGsensor : public QMainWindow
 
 public:
 	FBGsensor(QWidget *parent = Q_NULLPTR);
+	~FBGsensor();
 
+	public slots:
+	void on_openDeviceBtn_clicked();		//打开设备按钮槽函数，自动连接
+//已在designer中连接 	void on_openDeviceAct_triggered();		//打开设备动作槽函数，自动连接
+	void on_setParaBtn_clicked();			//设置设备参数槽函数，自动连接
 private:
 	Ui::FBGsensorClass ui;
+	QString portnumber = "COM3";		//存储端口号
+	quint32 waveStart = 1527000, waveEnd = 1568000, waveStep = 20;		//存储扫描波长起始值，终止值，间隔，单位为pm
+
 };
