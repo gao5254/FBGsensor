4b52ff4f524b93245fd724b9e207b2a6ef520e95
gao5254 <gao5254@qq.com>
2018-10-14 12:46:53 +0800

    编写坐标系转换函数

--- FBGsensor/displaylabel.cpp
+++ FBGsensor/displaylabel.cpp
@@ -19,6 +19,8 @@ void displayLabel::setPara(quint32 Sta, quint32 En, quint32 Ste, quint32 n, QVec
 	wStep = Ste;
 	n = chNum;
 	pData = p;
+
+	xBegin = wStart, xEnd = wEnd, yBegin = 0, yEnd = 4096;
 }
 
 void displayLabel::setIndex(int index)
@@ -40,74 +42,179 @@ void displayLabel::paintEvent(QPaintEvent *e)
 void displayLabel::drawCoordinateSys(QPainter *p)
 {
 	p->resetTransform();		//reset all transforms
-	QPen oriPen = p->pen();
+	QPen oriPen = p->pen();		//store the original pen
+	
 	//draw the main axis
 	QPen axisPen;
 	axisPen.setWidth(3);		//bold line to draw axis
 	p->setPen(axisPen);
 	p->drawLine(axisGap >> 1, this->height() - axisGap, this->width() - axisGap >> 1, this->height() - axisGap);	//x axis
 	p->drawLine(axisGap, this->height() - axisGap >> 1, axisGap, axisGap >> 1);		//y axis
-	int xinter = getXInterval(), yinter = getYInterval();
+	
 	//draw the dashline 
+	int xinter = getXInterval(), yinter = getYInterval();
+	int xlen = this->width() - axisGap - axisGap >> 1, ylen = this->height() - axisGap - axisGap >> 1;		//the display length in the widget
+	int xdis = xBegin - xEnd, ydis = yBegin - yEnd;		//the actual distance of the data
 	QPen dashPen;
 	dashPen.setStyle(Qt::DashLine);
 	dashPen.setBrush(Qt::darkGray);
 	p->setPen(dashPen);
 	//vertical line
-	int offsetx, offsety;
-	offsetx = xinter == 500 ? (xBegin / 500 + 1) * 500 : (xBegin / 1000 + 1) * 1000;
-	for (offsetx; offsetx < xEnd; offsetx += xinter)
+	for (int offsetx = xBegin; offsetx < xEnd; offsetx += xinter)
 	{
-		//TODO: continue
-		p->drawLine(axisGap + (offsetx - xBegin, this->height() - axisGap, axisGap, axisGap >> 1);		
+		
+		p->drawLine(axisGap + (offsetx - xBegin) * xlen / xdis, this->height() - axisGap, axisGap, axisGap >> 1);		
 
 	}
+
+	p->setPen(oriPen);		//reset the original pen
 }
 
 //transform the coordinate system according to the x_begin, x_end, y_begin, y_end
 void displayLabel::transformCoordinateSys(QPainter *p)
 {
-
+	//TODO: fix it
+	p->translate(axisGap, this->height() - axisGap);
+	p->translate(0 - (qint32)xBegin, 0 - (qint32)yBegin);
+	p->scale((qreal)(this->width() - axisGap - axisGap >> 1) / (qreal)(xBegin - xEnd);
 }
 
 //get the x axis interval(display scale) for dash-line drawing, depending on x_begin and x_end
 int displayLabel::getXInterval()
 {
 	int dis = xBegin - xEnd;
-	//if distance less than 500, internal is 500
-	if (dis <5000)
-	{
-		return 500;
-	}
-	//the default step is 6 or 7, test which interval is more close to the nearest 1000*n number
-	int inter6 = dis / 6, inter7 = dis / 7;
-	if (qMin(inter6 % 1000, 1000 - inter6 % 1000) <= qMin(inter7 % 1000, 1000 - inter7 % 1000))
-	{
-		return (inter6 + 500) / 1000 * 1000;
-	} 
-	else
-	{
-		return (inter7 + 500) / 1000 * 1000;
-	}
+// 	//if distance less than 500, internal is 500
+// 	if (dis <5000)
+// 	{
+// 		return 500;
+// 	}
+// 	//the default step is 6 or 7, test which interval is more close to the nearest 1000*n number
+// 	int inter6 = dis / 6, inter7 = dis / 7;
+// 	if (qMin(inter6 % 1000, 1000 - inter6 % 1000) <= qMin(inter7 % 1000, 1000 - inter7 % 1000))
+// 	{
+// 		return (inter6 + 500) / 1000 * 1000;
+// 	} 
+// 	else
+// 	{
+// 		return (inter7 + 500) / 1000 * 1000;
+// 	}
+// 	if (dis < 10)
+// 	{
+// 		return 1;
+// 	} 
+// 	else if (dis < 20)
+// 	{
+// 		return 2;
+// 	}
+// 	else if (dis < 50)
+// 	{
+// 		return 5;
+// 	} 
+// 	else if (dis < 100)
+// 	{
+// 		return 10;
+// 	}
+// 	else if (dis < 200)
+// 	{
+// 		return 20;
+// 	}
+// 	else if (dis < 500)
+// 	{
+// 		return 50;
+// 	}
+// 	else if (dis < 1000)
+// 	{
+// 		return 100;
+// 	}
+// 	else if (dis < 2000)
+// 	{
+// 		return 200;
+// 	}
+// 	else if (dis < 5000)
+// 	{
+// 		return 500;
+// 	}
+// 	else if (dis < 10000)
+// 	{
+// 		return 1000;
+// 	}
+// 	else if (dis < 20000)
+// 	{
+// 		return 2000;
+// 	}
+// 	else 
+// 	{
+// 		return 5000;
+// 	}
+
+	//the interval depends on the total distance, if dis is less than a number, then the interval is number/10
+	int arr[] = { 10, 20, 50, 100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000 };
+	int i = 0;
+	for (i = 0; dis >= arr[i]; i++);
+	return arr[i] / 10;
+
 }
 
 //get the y axis interval(display) for dash-line drawing, depending on y_begin and y_end
 int displayLabel::getYInterval()
 {
 	int dis = yBegin - yEnd;
-	//if distance less than 500, internal is 500
-	if (dis < 500)
-	{
-		return 50;
-	}
-	//the default step is 6 or 7, test which interval is more close to the nearest 1000*n number
-	int inter6 = dis / 6, inter7 = dis / 7;
-	if (qMin(inter6 % 100, 100 - inter6 % 100) <= qMin(inter7 % 100, 100 - inter7 % 100))
-	{
-		return (inter6 + 50) / 100 * 100;
-	}
-	else
-	{
-		return (inter7 + 50) / 100 * 100;
-	}
+// 	//if distance less than 500, internal is 500
+// 	if (dis < 500)
+// 	{
+// 		return 50;
+// 	}
+// 	//the default step is 6 or 7, test which interval is more close to the nearest 1000*n number
+// 	int inter6 = dis / 6, inter7 = dis / 7;
+// 	if (qMin(inter6 % 100, 100 - inter6 % 100) <= qMin(inter7 % 100, 100 - inter7 % 100))
+// 	{
+// 		return (inter6 + 50) / 100 * 100;
+// 	}
+// 	else
+// 	{
+// 		return (inter7 + 50) / 100 * 100;
+// 	}
+// 	if (dis < 10)
+// 	{
+// 		return 1;
+// 	}
+// 	else if (dis < 20)
+// 	{
+// 		return 2;
+// 	}
+// 	else if (dis < 50)
+// 	{
+// 		return 5;
+// 	}
+// 	else if (dis < 100)
+// 	{
+// 		return 10;
+// 	}
+// 	else if (dis < 200)
+// 	{
+// 		return 20;
+// 	}
+// 	else if (dis < 500)
+// 	{
+// 		return 50;
+// 	}
+// 	else if (dis < 1000)
+// 	{
+// 		return 100;
+// 	}
+// 	else if (dis < 2000)
+// 	{
+// 		return 200;
+// 	}
+// 	else 
+// 	{
+// 		return 500;
+// 	}
+
+	//the interval depends on the total distance
+	int arr[] = { 10, 20, 50, 100, 200, 500, 1000, 2000, 5000 };
+	int i = 0;
+	for (i = 0; dis >= arr[i]; i++);
+	return arr[i] / 10;
+
 }
