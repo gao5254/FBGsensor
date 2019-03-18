4b226265a14ca2afd051baa7be833ee0e15797d7
gao5254 <gao5254@qq.com>
2019-03-19 00:40:32 +0800

    阶段进展：读设置

--- FBGsensor/sensorinfowidget.cpp
+++ FBGsensor/sensorinfowidget.cpp
@@ -4,6 +4,28 @@ sensorInfoWidget::sensorInfoWidget(QWidget *parent)
 	: QWidget(parent)
 {
 	ui.setupUi(this);
+
+	// read the config file
+	QSettings settings("FBGconfig.ini", QSettings::IniFormat);
+	// test whether there has sensor
+	if (settings.contains("sensor"))
+	{
+		// load data to vector
+		int ssnum = settings.beginReadArray("sensor");
+		ssInfo = new QVector<sensorInfo>(ssnum);
+		for (int i = 0; i < ssnum; i++)
+		{
+			settings.setArrayIndex(i);
+			(*ssInfo)[i].ssName = settings.value("ssname").toString();
+			(*ssInfo)[i].chl = settings.value("chl").toInt();
+			(*ssInfo)[i].chl = static_cast<sensorType>(settings.value("chl").toInt());
+
+		}
+	} 
+	else
+	{
+	}
+
 }
 
 sensorInfoWidget::~sensorInfoWidget()
--- FBGsensor/sensorinfowidget.h
+++ FBGsensor/sensorinfowidget.h
@@ -1,7 +1,26 @@
 #pragma once
 
 #include <QWidget>
+#include <QSettings>
+#include <QVector>
 #include "ui_sensorinfowidget.h"
+
+// the type of the sensor
+enum sensorType
+{
+	Temperature, Pressure, Humidity
+};
+
+// to save the information of sensors
+struct  sensorInfo
+{
+	QString ssName;		//sensor name
+	quint8 chl;			//channel number
+	sensorType type;	//sensor type
+	quint32 wavRange[2];//rough range of the wavelength
+	double k;			//calibration parameter, k
+	double b;			//calibration parameter, b
+};
 
 class sensorInfoWidget : public QWidget
 {
@@ -10,6 +29,8 @@ class sensorInfoWidget : public QWidget
 public:
 	sensorInfoWidget(QWidget *parent = Q_NULLPTR);
 	~sensorInfoWidget();
+
+	QVector<sensorInfo> *ssInfo = nullptr;
 
 private:
 	Ui::sensorInfoWidget ui;
